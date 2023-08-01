ec8e16af2898ab3dceefba0cabb64c38031c9bc6
Alessio Leodori <alessio.leodori@gmail.com>
2023-08-01 15:24:29 +0200

    Added df examples

---
+++ Pandas/Examples/pandas_dataframe.py
@@ -0,0 +1,39 @@
+'''esempio di dataframes'''
+import pandas
+import pandas as pd
+
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+myvar = pd.DataFrame(data)
+
+#print(myvar)
+
+#print(myvar.loc[2])
+
+
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
+
+df = pd.DataFrame(data, index = ["day1", "day2", "day3"])
+
+#print(df)
+
+print(df.loc["day1"])
+
+print("-----------------------------------------")
+
+
+
+mydataset = {
+  'cars': ["BMW", "Volvo", "Ford"],
+  'passings': [3, 7, 2]
+}
+
+myvar = pandas.DataFrame(mydataset)
+
+print(myvar)
