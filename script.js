1a1c9ab8c711b446efeb384fd0109691d16fe4b0
Alex188dot <117444853+Alex188dot@users.noreply.github.com>
2023-09-16 08:32:53 +0200

    Updated script.js

--- script.js
+++ script.js
@@ -166,12 +166,6 @@ lang.addEventListener("click", function () {
   const personalProjects = document.querySelector(".personal-projects");
   const contactMe = document.querySelector("#contact-title");
   const card1 = document.querySelector("#card1");
-  const card2 = document.querySelector("#card2");
-  const card3 = document.querySelector("#card3");
-  const card4 = document.querySelector("#card4");
-  const card5 = document.querySelector("#card5");
-  const card6 = document.querySelector("#card6");
-  const card7 = document.querySelector("#card7");
   const card8 = document.querySelector("#card8");
   const card9 = document.querySelector("#card9");
   const card10 = document.querySelector("#card10");
@@ -184,12 +178,6 @@ lang.addEventListener("click", function () {
   const cardText2 = document.querySelector("#card-text2");
   const cardText3 = document.querySelector("#card-text3");
   const cardText4 = document.querySelector("#card-text4");
-  const cardText5 = document.querySelector("#card-text5");
-  const cardText6 = document.querySelector("#card-text6");
-  const cardText7 = document.querySelector("#card-text7");
-  const cardText8 = document.querySelector("#card-text8");
-  const cardText9 = document.querySelector("#card-text9");
-  const cardText10 = document.querySelector("#card-text10");
   const cardText11 = document.querySelector("#card-text11");
   const cardText12 = document.querySelector("#card-text12");
   const cardText13 = document.querySelector("#card-text13");
